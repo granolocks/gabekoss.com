c70779090eeaff9e3c917f33a976d6401bda15de
Gabe Koss <gabe@gabekoss.com>
2013-12-06 21:38:31 -0500

    start rendering partials

--- lib/default.rb
+++ lib/default.rb
@@ -1,8 +1,8 @@
 #include Nanoc3::Helpers::Blogging
 #include Nanoc3::Helpers::Tagging
-#include Nanoc3::Helpers::Rendering
 #include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::XMLSitemap
+include Nanoc3::Helpers::Rendering
 
 require 'pry'
 require 'builder'
