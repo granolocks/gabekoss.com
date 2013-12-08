4c93c9facc936313bb3160171dced2a979eab13f
Gabe Koss <gabe@gabekoss.com>
2013-12-07 21:26:18 -0500

    fix sitemap helper

---
+++ lib/sitemap_helper.rb
@@ -0,0 +1,13 @@
+module GabeKossDotCom
+  class SitemapHelper
+    def self.sitemap_items(items)
+      items.reject do |i|
+        i[:is_hidden] ||
+        i.binary? ||
+        i.path == '/sitemap.xml' ||
+        !( i.identifier.scan(/doge_blog\/\d{3}_doge/).empty? ) ||
+        !( i.path.scan(/api\/v1\/blog/).empty? )
+      end
+    end
+  end
+end
