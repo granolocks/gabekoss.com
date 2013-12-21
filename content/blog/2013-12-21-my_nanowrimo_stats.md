d884f6989ce20798caef66a220c260554e42bcd1
Gabe Koss <gabe@gabekoss.com>
2013-12-21 18:29:19 -0500

    noise

--- content/blog/2013-12-21-my_nanowrimo_stats.md
+++ content/blog/2013-12-21-my_nanowrimo_stats.md
@@ -4,7 +4,7 @@ created_at: 2013-12-21 15:12
 updated_at: 2013-12-21 15:12
 kind: blog-post
 author: Gabe Koss
-summary: 
+summary: A look at the progress I made on the National Novel writing Month challenge.
 tags: 
  - nanowrimo
 --- 
@@ -13,8 +13,102 @@ tags:
 <script src="/js/jquery-1-10-1-min.js"></script>
 <script src="/js/nano.js"></script>
 <style type="text/css">
-.wc-graph-bar:hover { fill: #C9FE51; }
+  .wc-graph-bar:hover { fill: #508320; }
+  #graph-wrapper {padding: 10px; background-color: rgba(20, 20, 20,0.2); } 
 </style>
 
-<div id="wc-line"></div>
-<div id="wc-graph"></div>
+On a total whim I decided to participate in [National Novel Writing
+Month](http://nanowrimo.org/).  This is a month long writing marathon in which
+particpants attempt to write a 50,000 word novel in the month of November. I
+cheated a little bit and started on October 26.
+
+<table>
+  <tr>
+    <td><strong>Total Words</strong></td>
+    <td>54173</td>
+  </tr>
+  <tr>
+    <td><strong>October Words</strong></td>
+    <td>2917</td>
+  </tr>
+  <tr>
+    <td><strong>November Words</strong></td>
+    <td>51256</td>
+  </tr>
+  <tr>
+    <td><strong>Avg Words/Day (Nov)</strong></td>
+    <td>1709</td>
+  </tr>
+</table>
+
+## Common Words
+
+After excluding common English stop words such as "that" or "is" the 10 most
+common words in my story were as follows:
+
+<table>
+  <tr><td><strong>sage</strong></td><td>631 instances</td></tr>
+  <tr><td><strong>out</strong></td><td>315 instances</td></tr>
+  <tr><td><strong>rama</strong></td><td>249 instances</td></tr>
+  <tr><td><strong>back</strong></td><td>184 instances</td></tr>
+  <tr><td><strong>one</strong></td><td>165 instances</td></tr>
+  <tr><td><strong>down</strong></td><td>144 instances</td></tr>
+  <tr><td><strong>looked</strong></td><td>139 instances</td></tr>
+  <tr><td><strong>here</strong></td><td>138 instances</td></tr>
+  <tr><td><strong>more</strong></td><td>125 instances</td></tr>
+  <tr><td><strong>know</strong></td><td>124 instances</td></tr>
+</table>
+
+## Common Bigrams
+
+Bigrams are two word units such as "depraved heathen" or "kind soul". The most
+common two word groupings were as follows:
+
+<table>
+  <tr><td><strong>of the</strong></td><td>390 instances</td></tr>
+  <tr><td><strong>in the</strong></td><td>222 instances</td></tr>
+  <tr><td><strong>to the</strong></td><td>188 instances</td></tr>
+  <tr><td><strong>on the</strong></td><td>163 instances</td></tr>
+  <tr><td><strong>into the</strong></td><td>151 instances</td></tr>
+  <tr><td><strong>she had</strong></td><td>107 instances</td></tr>
+  <tr><td><strong>was a</strong></td><td>104 instances</td></tr>
+  <tr><td><strong>from the</strong></td><td>92 instances</td></tr>
+  <tr><td><strong>out of</strong></td><td>91 instances</td></tr>
+  <tr><td><strong>she was</strong></td><td>90 instances</td></tr>
+</table>
+
+
+## Progress Over Time
+
+<div id="graph-wrapper">
+  <div id="wc-graph"></div>
+</div>
+
+Each bar indicates a date any will show you the exact number of words reached
+on that date. The light line is created from the word count at time I made
+a substantial save. 
+
+## Code snippets:
+
+I wrote the story with [Vim](/blog/2013/11/writing_with_vim/) and tracked my
+progress with Git. I did the analysis on this data using a combination of Ruby,
+D3.js and the Linux command line. Much of my data analysis was inspired by the
+classic [Unix for
+Poets](http://www.stanford.edu/class/cs124/kwc-unix-for-poets.pdf).
+
+Here are some of the tools I used to do this analysis.
+
+**Extract top 10 words:**
+
+```bash
+tr -sc '[A-Z][a-z]' '[\012*]' < story.md | tr '[A-Z]' '[a-z]' | sort | grep -E -v '^.{,2}$' | grep -E -v -f ../stop_words.grep |uniq -c | sort -n | tail -n 10
+```
+
+**Extract top 10 bigrams**
+
+```bash
+tr -sc '[A-Z][a-z]' '[\012*]' < story.md > nano.words                                   
+tail -c +2 nano.words > nano.next
+paste nano.words nano.next | sort | uniq -c | sort -n > nano.bigrams    
+tail -n 10 nano.bigrams
+```
--- content/js/nano.js
+++ content/js/nano.js
@@ -1,88 +1,110 @@
 $(document).ready(function(){
+  var fullWidth = document.getElementById('wc-graph').offsetWidth;
+  var graphPadding = 60;
+  var width = (fullWidth-graphPadding);
+  var height = 300;
+  var yTickCount = 20;
+  var yTickGap = height/yTickCount;
+
+  var edgeColor = "#000000";
+  var yCountColor = "#666666";
+  var barColor = "#506520";
+  var lineStroke = "#C9FE51";
+  var scaleLineColor = "lightgray"
+
+  // set up the SVG graph space
+  var palette = d3.select("#wc-graph").append("svg").
+    attr("width", fullWidth).
+    attr("height", height);
+
+  var lineContainer = palette.append("svg:g").
+    attr("x", graphPadding).
+    attr("height", height).
+    attr("width", width);
+
+  // set up object groups
+  var edgeGroup = lineContainer.append("svg:g");
+  var barGroup = lineContainer.append("svg:g");
+  var lineGroup = lineContainer.append("svg:g");
+
+  // Draw guides
+  edgeGroup.selectAll(".yTicks").
+    data(d3.range(0, yTickCount)).
+    enter().append("svg:line").
+    attr("x1", -10).
+    attr("x2", fullWidth).
+    attr("y1", function(d) { return height - (d*yTickGap); }).
+    attr("y2", function(d) { return height - (d*yTickGap); }).
+    attr("stroke", scaleLineColor).
+    attr("class", "yTicks");
+
+  // Draw Count
+  edgeGroup.selectAll(".yCount").
+    data( d3.range(1,yTickCount+1).reverse() ).
+    enter().append("svg:text").
+    // cheating here because i know the final max ;)
+    text(function(d){return(Math.floor(55000/d))}).
+    attr("x", 0).
+    attr("y", function(d,i) { return height - (i*yTickGap); }).
+    attr("fill", yCountColor)
+
+  // left edge
+  edgeGroup.append("svg:line").
+    attr("x1", graphPadding).
+    attr("x2", graphPadding).
+    attr("y1", 0).
+    attr("y2", height ).
+    attr("stroke", edgeColor).
+    attr("stroke-width", 1);
+  // botom edge
+  edgeGroup.append("svg:line").
+    attr("x1", graphPadding).
+    attr("x2", fullWidth).
+    attr("y1", height).
+    attr("y2", height).
+    attr("stroke", edgeColor).
+    attr("stroke-width", 2);
+
   $.getJSON('/json/nano_words_per_day.json', function(data){
-    var width = 900;
-    var height = 300;
-    var padding = 2;
-    var yTickCount = 20;
-    var yTickGap = height/yTickCount;
-
-    var x = d3.scale.linear().domain([0, data.length]).range([0, width]);
-    var y = d3.scale.linear().domain([0, d3.max(data, function(datum) { return datum[1]; })]).rangeRound([0, height]);
-
-    var lineFunction = d3.svg.line()
-      .x(function(d) { return data.indexOf(d) * (width/data.length); })
-      .y(function(d) { return height - y(d[1]); })
-      .interpolate("linear");
-
-    var lineContainer = d3.select("#wc-line").append("svg")
-      .attr("width", width)
-      .attr("height", height);
-
-    var axisGroup = lineContainer.append("svg:g");
-
-    var edges = lineContainer.append("svg:g");
-
-    // left edge
-    edges.append("svg:line").
-      attr("x1", 0).
-      attr("x2", 0).
-      attr("y1", 0).
-      attr("y2", height ).
-      attr("stroke", "black");
-
-    // botom edge
-    edges.append("svg:line").
-      attr("x1", 0).
-      attr("x2", width).
-      attr("y1", height).
-      attr("y2", height).
-      attr("stroke", "black");
-
-    axisGroup.selectAll(".yTicks").
-      data(d3.range(0, yTickCount)).
-      enter().append("svg:line").
-      attr("x1", -5).
-      attr("x2", width+5).
-      attr("y1", function(d) { return height - (d*yTickGap); }).
-      attr("y2", function(d) { return height - (d*yTickGap); }).
-      attr("stroke", "lightgray").
-      attr("class", "yTicks");
-
-    //The line SVG Path we draw
-    var lineGraph = lineContainer.append("path")
-      .attr("d", lineFunction(data))
-      .attr("stroke", "#506520")
-      .attr("stroke-width", 2)
-      .attr("fill", "none");
-  })
-
-  $.getJSON('/json/nano_stats.json', function(data){
-    var width = 900;
     var barWidth = width/data.length - 3;
-    var height = 300;
 
-    var x = d3.scale.linear().domain([0, data.length]).range([0, width]);
-    var y = d3.scale.linear().domain([0, d3.max(data, function(datum) { return datum.wc; })]).rangeRound([0, height]);
+    var x = d3.scale.linear().domain([0, data.length]).range([graphPadding, fullWidth]);
+    var y = d3.scale.linear().domain([0, d3.max(data, function(d) { return d[1]; })]).rangeRound([0, height]);
 
-    var wcBar = d3.select("#wc-graph").
-      append("svg:svg").
-      attr("width", width).
-      attr("height", height);
-
-    wcBar.selectAll("rect").
+    barGroup.selectAll("rect").
       data(data).
       enter().
       append("svg:rect").
-      attr("x", function(datum, index) { return x(index); }).
-      attr("y", function(datum) { return height - y(datum.wc); }).
-      attr("height", function(datum) { return y(datum.wc); }).
+      attr("x", function(d, index) { return x(index); }).
+      attr("y", function(d) { return height - y(d[1]); }).
+      attr("height", function(d) { return y(d[1]) - 1 ; }).
       attr("width", barWidth).
-      attr("fill", "#506520").
+      attr("fill", barColor).
       attr("class", "wc-graph-bar").
       attr("data-date", function(d){return d.date;}).
       attr("data-wc", function(d){return d.wc;}).
       attr("data-commit", function(d){return d.commit;}).
       append("svg:title").
-      text(function(d){ return (d.date+"\n"+d.wc+" words") }) ;
+      text(function(d){ return (d[0]+"\nWords:"+d[1])});
   })
+
+  $.getJSON('/json/nano_stats.json', function(data){
+
+    var x = d3.scale.linear().domain([0, data.length]).range([graphPadding, fullWidth]);
+    var y = d3.scale.linear().domain([0, d3.max(data, function(d) { return d.wc; })]).rangeRound([0, height]);
+
+
+    var lineFunction = d3.svg.line()
+      .x(function(d) { return data.indexOf(d) * (width/data.length) + graphPadding; })
+      .y(function(d) { return height - y(d.wc); })
+      .interpolate("linear");
+
+    //The line SVG Path we draw
+    var lineGraph = lineGroup.append("path").
+      attr("d", lineFunction(data)).
+      attr("stroke", lineStroke ).
+      attr("stroke-width", 2).
+      attr("fill", "none");
+  })
+
 });
