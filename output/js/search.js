207d06e279ab73bac91007cdccd42b0064dfea8a
Gabe Koss <gabe@gabekoss.com>
2013-12-21 23:39:14 -0500

    noise

--- output/js/search.js
+++ output/js/search.js
@@ -1,34 +1,41 @@
 var searchTerm;
 var searchParts;
 var results;
+
 $(document).ready(function(){
   $("#search").keyup(function(){
     results = [];
     mergedResults = [];
     searchTerm = $("#search").val();
-    searchParts = searchTerm.split(' ');
 
-    $(searchParts).each(function(i,tag){
-      selector = "li[name*='"+tag+"']";
-      x = $(selector);
-      results.push(x);
-    });
+    if( searchTerm != ""){
+      searchParts = searchTerm.split(' ');
 
-    $(results).each(function(i,resultsArray){
-      $(resultsArray).each(function(i,page){
-        if(mergedResults.indexOf(page) == -1){
-          mergedResults.push(page);
-        }
+      $(searchParts).each(function(i,tag){
+        selector = "li[name*='"+tag.toLowerCase()+"']";
+        x = $(selector);
+        results.push(x);
       });
-    });
 
+      $(results).each(function(i,resultsArray){
+        $(resultsArray).each(function(i,page){
+          if(mergedResults.indexOf(page) == -1){
+            mergedResults.push(page);
+          }
+        });
+      });
 
-    $("li.search-result").each(function(i,li){
-      if(mergedResults.indexOf(li) == -1){
-        $(li).hide();
-      } else {
+      $("li.search-result").each(function(i,li){
+        if(mergedResults.indexOf(li) == -1){
+          $(li).hide();
+        } else {
+          $(li).show();
+        };
+      });
+    } else {
+      $("li.search-result").each(function(i,li){
         $(li).show();
-      };
-    });
+      });
+    }
   });
 });
